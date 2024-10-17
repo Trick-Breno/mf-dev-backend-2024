970c16d40cf64261624a1f71c91331b8a1832566
Trick-Breno <141360060+Trick-Breno@users.noreply.github.com>
2024-10-17 16:27:16 -0300

    CRUD veiculos

--- mf-dev-backend-2024/Models/Veiculo.cs
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -16,9 +16,11 @@ namespace mf_dev_backend_2024.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar ano de fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar ano modelo!")]
+        [Display(Name ="Ano do modelo")]
         public int AnoModelo { get; set; }
     }
 }
